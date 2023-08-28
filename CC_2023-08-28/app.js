2911acac754335254e52251693e6f4c65107d989
Nia Pleasants <njpleasants@gmail.com>
2023-08-28 19:31:38 -0400

    Added Codewars challenge Convert a String to a Number 8kyu

---
+++ CC_2023-08-28/app.js
@@ -0,0 +1,9 @@
+//Codewars Challenge Convert a String to a Number! 8kyu
+//We need a function that can transform a string into a number. What ways of achieving this do you know?
+//P: Create a function that transforms a string into a number
+//R: Return a number
+//E:"1234" --> 1234
+//P:
+const stringToNumber = function(str){
+    return Number(str);
+  }
