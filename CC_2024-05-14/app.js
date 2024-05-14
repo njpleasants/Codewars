76c99b098f84b28a1cb23be88ceed9686e21d582
Nia Pleasants <njpleasants@gmail.com>
2024-05-14 17:05:44 -0400

    Adding Codewars challenge Basic Variable Assignment 8kyu

---
+++ CC_2024-05-14/app.js
@@ -0,0 +1,12 @@
+//Basic Variable Assignment 8kyu
+//This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+//P: Need to debug the code
+//R:
+//E:
+//P:
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
+
+
+
