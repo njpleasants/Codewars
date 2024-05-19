eb913986a133164b7ac92634a07b219fc5891007
Nia Pleasants <njpleasants@gmail.com>
2024-05-19 16:12:44 -0400

    Adding Codewars challenge Training JS 1: create your first JS function and print Hello World 8kyu

---
+++ CC_2024-05-19/app.js
@@ -0,0 +1,21 @@
+//Codewars challenge Training JS #1: create your first JS function and print "Hello World!" 8kyu
+//mission 1:
+// use keyword function to define your function, function name should be helloWorld(don't forget the () and {})
+//     mission 2:
+//     use keyword var (or let or const) to define a variable str, value of str should be a string: "Hello World!"(don't forget the =).
+//     mission 3:  
+//     type the console.log() in the next line (don't forget to put the str in the parentheses).   
+//     When you have finished the work, click "Run Tests" to see if your code is working properly.   
+//     In the end, click "Submit" to submit your code pass this kata.   
+//P: 
+//R:
+//E:
+//P:
+function helloWorld(){
+    let str = "Hello World!"
+    console.log(str)
+}
+
+//Other Codewars solutions
+helloWorld = () => /*var str*/ console.log('Hello World!')
+
