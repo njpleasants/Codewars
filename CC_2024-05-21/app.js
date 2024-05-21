edebad4e65907b96b8e12f389af85671fb483886
Nia Pleasants <njpleasants@gmail.com>
2024-05-21 19:27:31 -0400

    Adding Codewars challenge Get nth even number 8kyu

---
+++ CC_2024-05-21/app.js
@@ -0,0 +1,9 @@
+//Codewars challenge Get Nth Even Number 8kyu
+//Return the Nth Even Number
+//P: Will be given a number that is not 0 as the parameter.
+//R: Return the nth even number.
+//E: 3 --> 4 (the 3rd even number is 4 (0, 2, 4))
+//P:
+function nthEven(n){
+    return 2 * (n - 1);
+  }
