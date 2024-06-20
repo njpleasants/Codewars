25374784aa5fe71774e42c12fa574890d264e40f
Nia Pleasants <njpleasants@gmail.com>
2024-06-20 19:39:27 -0400

    Adding Codewars challenge summing a number's digits 7kyu

---
+++ CC_2024-06-20/app.js
@@ -0,0 +1,14 @@
+//Codewars challenge Summing a number's digits 7kyu
+//Write a function named sumDigits which takes a number as input and returns the sum of the absolute value of each of the number's decimal digits.
+//P:Will be given a number made up of two integers.
+//R:Zreturn the sum of the two integers.
+//E: 10 --> 1
+//P:
+function sumDigits(num) {
+    return Math.abs(num).toString().split("").reduce((total,next)=>total+Number(next),0)
+    }
+
+//Other Codewars solutions
+sumDigits = n => Math.abs(n).toString().split('').reduce((s, e) => s + +e, 0);
+
+
