a80fda99e17941d13556e2d17c06b29f2d3ccf73
Nia Pleasants <njpleasants@gmail.com>
2024-06-23 17:58:35 -0400

    Adding Codewars challenge Squaring an argument 8kyu

---
+++ CC_2024-06-23/app.js
@@ -0,0 +1,19 @@
+//Codewars challenge Squaring an argument 8kyu
+//Now you have to write a function that takes an argument and returns the square of it.
+//P:You will have a number to square
+//R: Return the square of the number
+//E: 
+//P:
+function square(num){
+   return num * num;
+  }
+
+//Other Codewars solutions
+
+const square = (n) => n * n;
+
+function square(x) {
+    return x ** 2;
+  }
+
+const square = a => a * a;
