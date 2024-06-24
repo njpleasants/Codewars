32e98876b866b6b7ef89a15315461d93f7443c08
Nia Pleasants <njpleasants@gmail.com>
2024-06-24 16:05:38 -0400

    Adding Codewars challenge Volume of a Cuboid 8kyu

---
+++ CC_2024-06-24/app.js
@@ -0,0 +1,18 @@
+//Codewars challenge Volume of a Cuboid
+//Bob needs a fast way to calculate the volume of a cuboid with three values: the length, width and height of the cuboid. Write a function to help Bob with this calculation.
+//P:Will be fiven three values 
+//R:Return the volume of the cuboid. 
+//E:Kata.getVolumeOfCuboid(1,2,2),  4)
+//P:
+class Kata {
+    static getVolumeOfCuboid(length, width, height) {
+      return length * width * height; 
+    }
+  }
+
+//Other Codewars solutions
+
+const Kata = {
+    getVolumeOfCuboid : (length, width, height) => length * width * height
+  }
+
