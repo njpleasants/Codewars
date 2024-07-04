4a7743768a88370d7c9384838fe0af47a416544f
Nia Pleasants <njpleasants@gmail.com>
2024-07-04 15:38:59 -0400

    Adding Codewars challenge Reverse order list 8kyu

---
+++ CC_2024-07-04/app.js
@@ -0,0 +1,13 @@
+//Reverse List Order 8kyu
+//In this kata you will create a function that takes in a list and returns a list with the reverse order.
+//P: Will be given an array of integers or strings to return in reverse order
+//R: Return the reverse of the given array.
+//E: [1, 2, 3, 4]  -> [4, 3, 2, 1]
+//P:
+function reverseList(list) {
+    return list.reverse();
+}
+
+//Other Codewars solutions
+const reverseList = list => list.reverse();
+
