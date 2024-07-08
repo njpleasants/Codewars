485bcb110a6455b37791c4682afa11cbf8702dc9
Nia Pleasants <njpleasants@gmail.com>
2024-07-08 16:33:20 -0400

    Adding Codewars challenge Count odd numbers below n 8kyu

---
+++ CC_2024-07-08/app.js
@@ -0,0 +1,14 @@
+//Count Odd Numbers below n 8kyu
+//Given a number n, return the number of positive odd numbers below n, EASY!
+//P: Will be given a positive number.
+//R: Return all the positive odd numbers below the given positive number.
+//E: 7  -> 3 (because odd numbers below 7 are [1, 3, 5])
+//P:
+function oddCount(n){
+  return Math.floor(n / 2);
+    }
+ 
+//Other Codewars solutions
+
+const oddCount = n => Math.floor(n/2) ;
+
