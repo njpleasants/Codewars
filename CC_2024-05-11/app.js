112b1bd0cca8572ca4c92f0335eef9c39b75e718
Nia Pleasants <njpleasants@gmail.com>
2024-07-29 15:58:45 -0400

    Edited for missing comma

--- CC_2024-05-11/app.js
+++ CC_2024-05-11/app.js
@@ -38,7 +38,7 @@ function howMuchILoveYou(n) {
 function howMuchILoveYou(nbPetals) {
     let phrase = {    
       0: "not at all",
-      1: "I love you"
+      1: "I love you",
       2: "a little",
       3: "a lot",
       4: "passionately",
