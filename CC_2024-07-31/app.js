91bf5efa6a80ec23ca6e4255e0ba7e0decf7d697
Nia Pleasants <njpleasants@gmail.com>
2024-08-02 17:17:08 -0400

    Additional save

--- CC_2024-07-31/app.js
+++ CC_2024-07-31/app.js
@@ -13,4 +13,30 @@ function shortcut (string) {
     }
     return result
 }
-  
+
+//Other Codewars solutions
+function shortcut (string) {
+    return string
+      .split('')
+      .filter(str => !'aeiou'.includes(str))
+      .join('')
+  }
+
+function shortcut(str) {
+    return str.split('').filter(function(e) {
+      return ['a', 'e', 'i', 'o', 'u'].indexOf(e) == -1 
+    }).join('')
+  }
+
+function shortcut(string){
+    var vowels = "aeiou";
+    var output = "";
+    for (var i = 0; i < string.length; i++) {
+      if (!(vowels.indexOf(string[i]) > -1)) {
+        output += string[i];
+      }
+    }
+    return output;
+  }
+
+
