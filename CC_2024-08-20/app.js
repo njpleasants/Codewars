5680a553c39b445e28dcda3f28686ae8cbc37106
Nia Pleasants <njpleasants@gmail.com>
2024-08-20 19:14:58 -0400

    Adding Codewars challenge 5 without numbers 8kyu

---
+++ CC_2024-08-20/app.js
@@ -0,0 +1,23 @@
+//Codewars challenge 5 Without Numbers!! 8kyu
+//Write a function that always returns 5
+//Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+//P: Will not be able to use 0123456789*+-/ to produce the number 5.
+//R: Need to always return the number 5.
+//E: 
+//P:
+function unusualFive() {
+    return ['apple','banana','carrot','dragonfruit','eggplant'].length;
+  }
+
+//Other Codewars solutions
+function unusualFive() {
+    return "hello".length;
+  }
+
+function unusualFive() {
+    let string = ["a", "b", "c", "d", "e", "f"];
+    let search = "f";
+    let indexOfFirst = string.indexOf(search);
+    return indexOfFirst;
+  }
+
