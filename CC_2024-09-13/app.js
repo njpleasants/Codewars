1c7fbd64cefd19cd570edb653e9e3eb2a12332c4
Nia Pleasants <njpleasants@gmail.com>
2024-09-13 14:10:22 -0400

    Adding Codewars challenge Grosshoper Function syntax and debugging 8kyu

---
+++ CC_2024-09-13/app.js
@@ -0,0 +1,18 @@
+//Codewars challenge Grasshopper - Function syntax debugging 8kyu
+//A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
+//P:
+//R:
+//E:
+//P:
+//Given code: function main [verb, noun]
+//   return verb + noun
+// }
+
+//Solution
+function main(verb, noun){
+    return verb + noun;
+}
+
+//Other solution
+
+const main = (verb, noun) => verb + noun;
