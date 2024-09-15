b4c11035369aa40ccd0eec26c399f3eaa407f047
Nia Pleasants <njpleasants@gmail.com>
2024-09-15 16:49:18 -0400

    Adding Codewars challenge Hex to Decimal 8kyu

---
+++ CC_2024-09-15/app.js
@@ -0,0 +1,14 @@
+//Codewars challenge Hex to Decimal 8kyu
+//Complete the function which converts hex number (given as a string) to a decimal number.
+//P:
+//R:
+//E:
+//P:
+function hexToDec(hexString){
+    return parseInt(hexString, 16);
+  }
+
+//Other solutions
+
+const hexToDec = h => parseInt(h, 16);
+
