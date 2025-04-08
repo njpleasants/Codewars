0810a872a08791915f78af1cc78b74cd6ad3e0db
Nia Pleasants <njpleasants@gmail.com>
2025-04-08 18:58:34 -0400

    Adding Codewars challenge Grasshopper Messi Goals 8kyu

---
+++ CC_2025-04-08/app.js
@@ -0,0 +1,25 @@
+//Codewars challenge Grasshopper - Messi Goals
+//
+//P:Use variables to find the sum of the goals Messi scored in 3 competitions
+
+// Information
+// Messi goal scoring statistics:
+
+// Competition	Goals
+// La Liga	43
+// Champions League	10
+// Copa del Rey	5
+// Task
+// Create these three variables and store the appropriate values using the table above:
+// laLigaGoals
+// championsLeagueGoals
+// copaDelReyGoals
+// Create a fourth variable named totalGoals that stores the sum of all of Messi's goals for this year.
+//R: Return the number of goals for the year stored as the fourth variable totalGoals`
+//E:
+//P:
+var laLigaGoals = 43
+var championsLeagueGoals = 10
+var copaDelReyGoals = 5
+
+var totalGoals = laLigaGoals + championsLeagueGoals + copaDelReyGoals
