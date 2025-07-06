31b19215379a6e888951e534891256c07e3a1495
Nia Pleasants <njpleasants@gmail.com>
2025-07-06 17:25:48 -0400

    Adding Codewars challenge Fundamentals Return 8kyu

---
+++ CC_2025-07-06/app.js
@@ -0,0 +1,54 @@
+//Codewars challenge Fundamentals : Return 8kyu
+//Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+
+// Please use the following function names:
+
+// addition = add
+
+// multiply = multiply
+
+// division = divide (both integer and float divisions are accepted)
+
+// modulus = mod
+
+// exponential = exponent
+
+// subtraction = subt
+
+//P: Given various functions with mathematical problems, return the approprirate answer.
+//R: Return answers for each function.
+//E: 
+//P:
+function add(a,b){
+    return (a + b)
+}
+
+function divide(a,b){
+    return (a / b)
+}
+
+function multiply(a,b){
+    return (a * b)
+}
+
+function mod(a,b){
+    return (a % b)
+}
+   
+function exponent(a,b){
+    return (a ** b)
+}
+    
+function subt(a,b){
+    return (a - b)
+}
+
+//Other solutions
+
+const add = (a, b) => a + b;
+const subt = (a, b) => a - b;
+const divide = (a, b) => a / b;
+const multiply = (a, b) => a * b;
+const mod = (a, b) => a % b;
+const exponent = (a, b) => a ** b;
+
