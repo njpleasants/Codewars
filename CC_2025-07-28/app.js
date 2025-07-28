23989dd8f21e46b791edfe87a4de5b5af7ce9029
Nia Pleasants <njpleasants@gmail.com>
2025-07-28 18:20:36 -0400

    Adding Codewars challenge Basic Training Add Item to an Array 8kyu

---
+++ CC_2025-07-28/app.js
@@ -0,0 +1,10 @@
+//Codewars challenge Basic Training: Add item to an Array 8kyu
+//Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+
+// The websites array has already been defined for you using the following code:
+//P: Given the variable websites, return the array with "codewars"
+//R: Return the array websites so it == "codewars"
+//E:
+//P:
+websites.push('codewars');
