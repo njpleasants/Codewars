4fee9d228085910376f749bdab93a3363f7705a0
Nia Pleasants <njpleasants@gmail.com>
2025-08-05 16:25:54 -0400

    Adding Codewars challenge Training JS3 Basic Data Types String 8kyu

---
+++ CC_2025-08-05/app.js
@@ -0,0 +1,80 @@
+//Codewars challenge Training JS #3: Basic data types--String 8kyu
+//misson 1: I've create three function, and defined some global variables, please select some variables that can make up the name of the function, and return them(Please note the uppercase and lowercase letters are different).
+
+// When you have finished the work, click "Run Tests" to see if your code is working properly.
+
+// misson 2: After misson 1 finished. you should click "Attempt" to see my three questions, and write the answer in function answer1, answer2,answer3
+
+// If everything is right, click "Submit" again to submit your code pass this kata.
+
+//P: Create functions whose variable spell out the name of the function in string form.
+//R: Return the functions.
+//E:
+//P:
+
+var a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+2
+function Dad(){
+3
+//   let d1 = 'D'
+4
+//   let a2 = 'a'
+5
+//   let d2 = 'd'
+6
+  return d1+a2+d2;
+7
+}
+8
+function Bee(){
+9
+  return b1+e2+e2;
+10
+}
+11
+function banana(){
+12
+  return b2+a2+n2+a2+n2+a2;
+13
+}
+14
+​
+15
+//answer some questions if you finished works above
+16
+function answer1(){
+17
+  //the answer should be "yes" or "no"
+18
+  return "no";
+19
+}
+20
+function answer2(){
+21
+  //the answer should be "yes" or "no"
+22
+  return "no";
+23
+}
+24
+function answer3(){
+25
+  //the answer should be "yes" or "no"
+26
+  return "yes";
+27
+}
+28
+​
+29
+​
+
+//Other solutions
+const Dad = () => d1 + a2 + d2
+const Bee = () => b1 + e2 + e2
+const banana = () => b2 + a2 + n2 + a2 + n2 + a2
+
+const answer1 = () => "no"
+const answer2 = () => "no"
+const answer3 = () => "yes"
